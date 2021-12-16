a7a66ad6ab497bad746ab19a280709adedaf3711
joelhernandezvasquez <jhernandez98@outlook.es>
2021-12-16 14:42:02 -0500

    first deplou

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [task,setTask] = useState("");
+  const [todos,setTodos] = useState([]);
+  
+const markItem = () =>{
+   return{
+
+   }
+}
+  const createTodos = () =>{
+    if(task!=="")
+    {
+      setTodos([...todos,task]);
+      setTask("");
+    }
+  }
+
+  const deleteTask = (e,index) =>{
+   
+    
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+     <div className='todo-container'>
+    
+       <div className='input-container'>
+         <input type = "text"
+          placeholder='Write A task'
+          value = {task}
+          onChange={(e)=> setTask(e.target.value)}
+         />
+         <button className='btn-create-task' onClick={(e)=> createTodos()}>
+           Create a Task
+         </button>
+       </div>
+
+     <div className='todos-section'>
+     {todos.map((todo,index)=>{
+         return <div key={index} className='todo-item' onClick={(e)=> deleteTask(e,index)}> <p> {todo}</p></div>
+       })}
+     </div>
+     
+     </div>
     </div>
   );
 }
